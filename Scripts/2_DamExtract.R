d6e0d492d8824cc79a2858cf8dc4beb9e89316e2
MatthiasSiewert <msiewert@gmx.de>
2020-11-08 08:55:28 +0100

    script2

--- Scripts/2_DamExtract.R
+++ Scripts/2_DamExtract.R
@@ -9,15 +9,15 @@
 ###
 
 library(rgdal)
-# library(tidyverse)
+ library(tidyverse)
  library(lubridate)
-# library(raster)
-# library(readODS)
-# library(cowplot)
+ library(raster)
+ library(readODS)
+ library(cowplot)
 # library(wesanderson)
-# library(ggsci)
+ library(ggsci)
 # #library(doBy)
-# library(ggpmisc)
+ library(ggpmisc)
 
 
 
@@ -58,7 +58,7 @@ DamList <- list()
 for (i in 1:length(Rastlist)){
   temp <- raster::extract(raster(Rastlist[[i]]),DamPlots)
   names(temp) <- DamPlots$name
-  name <- substr(Rastlist[[i]],24,150)
+  name <- substr(Rastlist[[i]],20,150)
   temp <- Filter(Negate(is.null), temp)
   DamList[[name]] <- temp
   print(name)}
@@ -92,12 +92,12 @@ meanTable$plot <- substr(meanTable$names, nchar(meanTable$names)-4, nchar(meanTa
 meanTable$type <- substr(meanTable$names, nchar(meanTable$names)-1, nchar(meanTable$names)-1)
 
 #meanTable$season <- ifelse(month(meanTable$date) == 6, 'spring', ifelse(month(meanTable$date) == 9, 'autumn', 'peak')) 
-meanTable$year   <- ifelse(year(meanTable$date) == 2018, 2018, ifelse(year(meanTable$date) == 2019, 2019, 2017)) 
+meanTable$year   <- ifelse(year(meanTable$date) == 2018, 2018, ifelse(year(meanTable$date) == 2019, 2019, NA)) 
 
 # only use 2018 and 2019
 meanTable <- subset(meanTable, date > as.Date("2018-01-01"))
 meanTable
-write_csv(meanTable, 'meanTable.csv')
+write_csv(meanTable, 'Table/meanTable.csv')
 
 
 ##### extract the mean of all Damage and all control plots per study area
@@ -107,41 +107,68 @@ meanTable2 <- meanTable %>%
 meanTable2
 
 meanTable2$season <- ifelse(month(meanTable2$date) == 6, 'spring', ifelse(month(meanTable2$date) == 9, 'autumn', 'peak')) 
-meanTable2$year   <- ifelse(year(meanTable2$date) == 2018, 2018, ifelse(year(meanTable2$date) == 2019, 2019, 2017)) 
-meanTable2 <- subset(meanTable2, date > as.Date("2018-01-01"))
+meanTable2$year   <- ifelse(year(meanTable2$date) == 2018, 2018, ifelse(year(meanTable2$date) == 2019, 2019, NA)) 
+#meanTable2 <- subset(meanTable2, date > as.Date("2018-01-01"))
 meanTable2
 
 ############################################################## combine with damage information
-## Load ground inventory data
-#sumTable <- read_csv('~/Data/Drones/Damage/DamageR/sumTablerules3_eachRodentcounts1max1.csv')
-sumTable <- read_csv('~/Data/Drones/Damage/DamageR/sumTablerules4_eachRodentcounts1nomaxbut3.csv')
+## Load digitized ground inventory data
 
-# also merge class names
-temp <- read_ods('~/Data/Drones/Damage/DamageR/Excel/PlotTable.ods')
-temp[, c('plot', 'habitat')]
-temp$habitat = factor(temp$habitat,levels = c("Dry heath & Barren",
+dmOut <- readRDS("Table/dmOut.rds")
+
+dmOut
+########## create a Summary table
+# sums
+sum(dmOut[[4]]) # test count for one plot
+temp <- lapply(dmOut, sum)
+
+sumTable <- as.data.frame(do.call(rbind, temp))
+colnames(sumTable) <- "damCount"
+sumTable$plot <- rownames(sumTable)
+sumTable$type <-substr(sumTable$plot,4,4)
+sumTable$area <-substr(sumTable$plot,1,2)
+
+ggplot(sumTable, aes(plot, damCount)) +
+  geom_bar( stat = "identity", aes(fill = type))
+
+# damage for a specific study area
+temp <- sumTable %>% 
+  group_by(area, type) %>%
+  summarise(mean_impact = mean(damCount))
+
+ggplot(temp, aes(area, mean_impact)) +
+  geom_bar( stat = "identity", aes(fill = type), position = position_dodge())
+
+### create a tibble
+sumTable <- as_tibble(sumTable)
+
+###################################33
+# load habitat classes
+habitatTable <- read_csv('Table/habitatTable.csv')
+
+habitatTable
+habitatTable$habitat = factor(habitatTable$habitat,levels = c("Dry heath & Barren",
                                               "Betula nana heath",
                                               "Mesic heath & Snowbed",
                                               "Willow",
                                               "Birch forest",
-                                              "Wetlands & meadows"
-))
+                                              "Wetlands & meadows"))
 
 
-sumTable <- merge(sumTable, temp[, c('plot', 'habitat')], by = 'plot')
+sumTable <- merge(sumTable, habitatTable[, c('plot', 'habitat')], by = 'plot')
 
 sumTable
 meanTable
 
-meanTable3 <- meanTable
+temp <- meanTable
 
 # clean up
-meanTable3$names <- NULL
-meanTable3$season = factor(meanTable3$season,levels = c("spring", "peak", "autumn"))
+temp$names <- NULL
+#meanTable3$season = factor(meanTable3$season,levels = c("spring", "peak", "autumn"))
 
 # Need to delete date, otherwise pivot wont work. 
-meanTable3$date <- NULL
-temp <-meanTable3 %>% pivot_wider(names_from = year, values_from = meanNDVI)
+temp$date <- NULL
+temp <-temp %>% pivot_wider(names_from = year, values_from = meanNDVI)
 temp
 
 # combine with previous results
@@ -149,7 +176,7 @@ temp2 <- merge(temp,sumTable, by = c( 'area', 'plot','type'))
 temp2$X1 <- NULL
 
 # Subtract NDVI 2019 minus 2018
-colnames(temp2) <- c("area","plot","type", "season","y2018","y2019","damCount","habitat")
+colnames(temp2) <- c("area","plot","type", "y2018","y2019","damCount","habitat")
 temp2$y2018 <- as.numeric(as.character(temp2$y2018))
 temp2$y2019 <- as.numeric(as.character(temp2$y2019))
 
@@ -157,21 +184,21 @@ temp2$diff19minus18 <- temp2$y2019 - temp2$y2018
 temp2
 
 # use only peak season
-temp <- subset(temp2 , season == 'peak')
-#temp <- subset(temp, plot != c('KJ-C1' , 'KJ-D1'))
+#temp <- subset(temp2 , season == 'peak')
+#temp <- subset(temp2, plot != c('KJ-C1' , 'KJ-D1'))
 
-damageTable <- temp
+damageTable <- temp2
 damageTable
 summary(lm(damCount ~ diff19minus18 , data = damageTable))
-summary(lm(damCount ~ diff19minus18:area , data = damageTable))
+# summary(lm(damCount ~ diff19minus18:area , data = damageTable))
+# 
+# summary(lm(damCount ~ diff19minus18:habitat , data = damageTable))
+# 
+# summary(lm(damCount ~ diff19minus18 , data = subset(damageTable, habitat %in% c("Dry tundra & Barren" )) ))
+# summary(lm(damCount ~ diff19minus18 , data = subset(damageTable, habitat %in% c("Mesic heath" )) ))
 
-summary(lm(damCount ~ diff19minus18:habitat , data = damageTable))
 
-summary(lm(damCount ~ diff19minus18 , data = subset(damageTable, habitat %in% c("Dry tundra & Barren" )) ))
-summary(lm(damCount ~ diff19minus18 , data = subset(damageTable, habitat %in% c("Mesic heath" )) ))
-
-
-
+# All plots
 
 Fig2_damPlot_all <- ggplot(damageTable, aes(x=damCount, y = diff19minus18)) +
   geom_smooth(method=lm, se=T, color = 'black') +
@@ -188,8 +215,7 @@ Fig2_damPlot_all <- ggplot(damageTable, aes(x=damCount, y = diff19minus18)) +
                parse = TRUE, size =2.5)   
 Fig2_damPlot_all
 
-ggsave("Plots/Fig_2_Damage/Fig2_damPlot_all.pdf", Fig2_damPlot_all,  width = 8, height = 8, units = "cm")
-
+# Plots per habitat
 Fig2_damPlot_sep <- ggplot(damageTable, aes(x=damCount, y = diff19minus18)) +
   geom_smooth(method=lm, se=T, color = 'black') +
   #   geom_point() +
@@ -205,25 +231,21 @@ Fig2_damPlot_sep <- ggplot(damageTable, aes(x=damCount, y = diff19minus18)) +
                parse = TRUE, size =2.5)   
 Fig2_damPlot_sep
 
-tempPlot <- ggplot(damageTable, aes(x=damCount, y = diff19minus18)) +
-  geom_smooth(method=lm, se=T, color = 'black') +
-  geom_point(aes(col =habitat)) +
-  #  geom_point(aes(col =area)) +
-  #  geom_text(aes(label = plot, size =6)) +
-  theme_classic() +
-  #  scale_color_npg() +
-  theme(legend.position="none") +
-  labs(y= "UAV NDVI change 2019 -  2018", x = "Field impact inventory count") +
-  facet_wrap(~area)#, scales = "free_y")
-tempPlot
+# tempPlot <- ggplot(damageTable, aes(x=damCount, y = diff19minus18)) +
+#   geom_smooth(method=lm, se=T, color = 'black') +
+#   geom_point(aes(col =habitat)) +
+#   #  geom_point(aes(col =area)) +
+#   #  geom_text(aes(label = plot, size =6)) +
+#   theme_classic() +
+#   #  scale_color_npg() +
+#   theme(legend.position="none") +
+#   labs(y= "UAV NDVI change 2019 -  2018", x = "Field impact inventory count") +
+#   facet_wrap(~area)#, scales = "free_y")
+# tempPlot
 
-
-
-#plotRule1 <- tempPlot
-#plotRule3 <- tempPlot
 
 Fig2_damPlot_comp <- plot_grid(Fig2_damPlot_all,Fig2_damPlot_sep,rel_widths = c(1, 1), labels = c("a","b"))
 Fig2_damPlot_comp
 
-ggsave("Plots/Fig_2_Damage/Fig2_damPlot_comp.pdf", Fig2_damPlot_comp,  width = 25, height = 12, units = "cm")
-ggsave("Plots/Fig_2_Damage/Fig2_damPlot_comp.png", Fig2_damPlot_comp,  width = 25, height = 12, units = "cm")
+ggsave("Plots/Fig_2_NDVIvsImpact/Fig_2_NDVIvsImpact.pdf", Fig2_damPlot_comp,  width = 25, height = 12, units = "cm")
+ggsave("Plots/Fig_2_NDVIvsImpact/Fig_2_NDVIvsImpact.png", Fig2_damPlot_comp,  width = 25, height = 12, units = "cm")
